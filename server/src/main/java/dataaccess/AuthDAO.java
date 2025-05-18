18852d5b0ae11e8cbfa660a6649d73eddf772ca9
Ryan <rmort03@byu.edu>
2025-05-17 23:40:46 -0400

    Added DataAccessException to AuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,10 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    void addAuth(AuthData authData) throws  DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void removeAuth(String authToken) throws DataAccessException;
+    void clear();
 }
--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,45 @@
 package dataaccess;
 
+import model.AuthData;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryAuthDAO implements AuthDAO{
+    private final Map<String, AuthData> storage;
+
+    MemoryAuthDAO() {
+        this.storage = new HashMap<>();
+    }
+
+    @Override
+    public void addAuth(AuthData authData) throws DataAccessException{
+        String token = authData.authToken();
+        if (storage.containsKey(token)) {
+            throw new DataAccessException("Auth token exists.");
+        }
+        storage.put(token, authData);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        AuthData auth = storage.get(authToken);
+        if (auth == null) {
+            throw new DataAccessException("Invalid auth token.");
+        }
+        return auth;
+    }
+
+    @Override
+    public void removeAuth(String authToken) throws DataAccessException {
+        if (storage.remove(authToken) == null) {
+            throw new DataAccessException("Auth token not found.");
+        }
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
 }
