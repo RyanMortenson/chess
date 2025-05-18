3657ffba53a67d60822668bff04a62d9e198ecc6
Ryan <rmort03@byu.edu>
2025-05-18 00:03:19 -0400

    Implemented GameDAO

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,13 @@
 package dataaccess;
 
+import model.GameData;
+import java.util.List;
+
 public interface GameDAO {
+
+    int createGame(GameData game);
+    GameData getGame(int gameID) throws DataAccessException;
+    List<GameData> listGames();
+    void updateGame(int gameID, GameData updated) throws DataAccessException;
+    void clear();
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,59 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.*;
+
 public class MemoryGameDAO implements GameDAO{
+    private final Map<Integer, GameData> storage;
+    private int nextID;
+
+    MemoryGameDAO() {
+        this.storage = new HashMap<>();
+        this.nextID = 1;
+    }
+
+    @Override
+    public int createGame(GameData game) {
+        int id = nextID++;
+        GameData withID = new GameData(id,
+                game.whiteUsername(),
+                game.blackUsername(),
+                game.gameName(),
+                game.game());
+        storage.put(id, withID);
+        return id;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        GameData game = storage.get(gameID);
+        if (game == null) {
+            throw new DataAccessException("Game does not exist: " + gameID);
+        }
+        return game;
+    }
+
+    @Override
+    public List<GameData> listGames() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData updated) throws DataAccessException {
+        if (!storage.containsKey(gameID)) {
+            throw new DataAccessException("Game "+gameID+" not found.");
+        }
+        storage.put(gameID, new GameData(gameID,
+                updated.whiteUsername(),
+                updated.blackUsername(),
+                updated.gameName(),
+                updated.game()));
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+        nextID=1;
+    }
 }
