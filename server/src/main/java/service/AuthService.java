320def1349e16244b57575232b07c01446637d74
Ryan <rmort03@byu.edu>
2025-05-18 19:37:32 -0400

    Built out AuthService

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -1,4 +1,35 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import java.util.UUID;
+
 public class AuthService {
+    private final AuthDAO authDao;
+
+    public AuthService(AuthDAO authDao) {
+        this.authDao = authDao;
+    }
+
+    public AuthData issueToken(String username) throws DataAccessException {
+        String token = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(token, username);
+        authDao.addAuth(auth);
+        return auth;
+    }
+
+    public AuthData validateToken(String authToken) throws DataAccessException {
+        if (authToken == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        return authDao.getAuth(authToken);
+    }
+
+    public void revokeToken(String authToken) throws DataAccessException {
+        if (authToken == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        authDao.removeAuth(authToken);
+    }
 }
