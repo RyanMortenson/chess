90eb4a1bb314dcf76e3b939e7f0b5eaa9ca368cd
Ryan <rmort03@byu.edu>
2025-05-18 22:02:46 -0400

    Added DBService for dataBase

---
+++ server/src/main/java/service/DBService.java
@@ -0,0 +1,23 @@
+package service;
+
+import dataaccess.UserDAO;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+
+public class DBService {
+    private final UserDAO userDao;
+    private final AuthDAO authDao;
+    private final GameDAO gameDao;
+
+    public DBService(UserDAO u, AuthDAO a, GameDAO g) {
+        this.userDao = u;
+        this.authDao = a;
+        this.gameDao = g;
+    }
+
+    public void clear() {
+        userDao.clear();
+        authDao.clear();
+        gameDao.clear();
+    }
+}
